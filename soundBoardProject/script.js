25a2733e2e655a704056e7845c393ce0f5623012
d25coder <jamerria15@yahoo.com>
2026-05-18 18:34:50 -0400

    added event listener functions to script,js file

--- soundBoardProject/script.js
+++ soundBoardProject/script.js
@@ -1,1 +1,27 @@
+function playSound(key) {
+
+const audio = document.querySelector(`audio[data-key="${key}"]`);
+const keyItem = document.querySelector(`li[data-key="{key}"]`);
+
+if (!audio) return;
+
+audio.currentTime = 0;
+audio.play();
+
+keyItem.classList.add('playing');
+setTimeout(function() {
+    keyItem.classList.remove('playing');
+}, 100);
+
+}
+
+window.addEventListener('keydown', function(event) {
+    plsySound(event.key.toLowerCase());
+});
+
+document.querySelector('.keys').addEventListener('click',function(event) {
+    const keyItem = event.target.closest('li[data-key]');
+    if (!keyItem) return;
+    playSound(keyItem.dataset.key);
+});
 
